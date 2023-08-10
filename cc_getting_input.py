8cd79851f0b9bbc3a7b646ec52032240d767f59f
Morprok <141086733+Morprok@users.noreply.github.com>
2023-08-10 12:26:58 +1200

    Add files via upload

---
+++ cc_getting_input.py
@@ -0,0 +1,13 @@
+username = input("What is your name? ")
+
+fav_num = int(input("Favourite numbers? "))
+
+double = fav_num * 2
+half = fav_num / 2
+squared = fav_num * fav_num
+
+print("Hi {}, your favourite number is {}". format(username, fav_num))
+
+print("double {} is {}".format(fav_num, double))
+print("half of {} is {}".format(fav_num, half))
+print("{} squared is {}".format(fav_num, squared))
